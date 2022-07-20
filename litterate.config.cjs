c1075a2fbd83311ce9c1d297d573795924956c66
Linus Lee <linus@thesephist.com>
2022-07-20 14:44:29 -0400

    docs: Rebuild docs with updated litterate version

--- litterate.config.cjs
+++ litterate.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
     description: '**libsearch** is a fast, small, minimal full-text search library used extensively across my personal projects. In my experience, it\'s just the easiest way to add search capabilities to your app that goes beyond substring matches without bringing in too much complexity. Read more at [the GitHub repo](https://github.com/thesephist/libsearch).',
     baseURL: '/libsearch',
     files: [
-        './lib/**/*.ts',
+        './lib/**/*',
+        './test/**/*',
     ],
 }
